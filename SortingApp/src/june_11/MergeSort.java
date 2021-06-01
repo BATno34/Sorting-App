84a65c6045fbb6182e3652a9a0e9c59beb7de095
Shreyas <Shreyas@192.168.2.16>
2021-06-01 13:55:06 -0400

    merge sort

---
+++ SortingApp/src/june_11/MergeSort.java
@@ -0,0 +1,124 @@
+package june_11;
+
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class MergeSort implements Runnable {
+	public static JLabel[] bars;
+	public static int [] barsHeight;
+	
+	public MergeSort(JLabel[] bars, int[] barsHeight) {
+		this.bars = bars;
+		this.barsHeight = barsHeight;
+	}
+
+	@Override
+	public void run() {
+		mergeSort(0, bars.length-1);
+	}
+
+	/**
+	 * merges two subarrays of an array (for merge sort)
+	 * @param lo, the lowest index of the part of the array to be sorted
+	 * @param mid, the mid point of the part of the array to be sorted
+	 * @param hi, the highest index of the part of the array to be sorted
+	 */
+	private void merge(int lo, int mid, int hi) {
+		ArrayList<Integer> mergedSorted = new ArrayList<Integer>();
+		ArrayList<Integer> important = new ArrayList<Integer>();
+		ArrayList<Integer> sorted = new ArrayList<Integer>();
+		
+		int i = lo;
+		int j = mid + 1;
+		
+		while(i <= mid && j <= hi) {
+			if(barsHeight[i] < barsHeight[j]) { //if the current item in the lower half is smaller than the current item in the upper half
+				mergedSorted.add(barsHeight[i]);
+				i++;
+			} else {//if the current item in the upper half is smaller than the current item in the lower half
+				mergedSorted.add(barsHeight[j]);
+				j++;
+			}
+		}
+		
+		//copy any other elements remaining in the lower half
+		while(i<=mid) {
+			mergedSorted.add(barsHeight[i]);
+			i++;
+		}
+		
+		//copy any other elements remaining in the upper half
+		while(j<=hi) {
+			mergedSorted.add(barsHeight[j]);
+			j++;
+		}
+		
+		//copy mergedSorted into main list
+		i = 0;
+		j = lo;
+
+		while(i < mergedSorted.size()) {
+			important.add(i);
+			important.add(j);
+			
+			barsHeight[j] = mergedSorted.get(i);
+			SortingAlgorithmsFunction.drawSortedBars(important,sorted);
+			 try {
+				 Thread.sleep(100);
+			 } catch (InterruptedException e) {
+				 e.printStackTrace();
+		   	 }
+			
+			i++;
+			j++;
+		}
+		
+		if(lo == 0 && hi == barsHeight.length-1) {
+			SortingAlgorithmsFunction.endOfSort();
+			JOptionPane.showMessageDialog(null, "Sorting complete!");
+		}
+	}
+	
+	/**
+	 * Sorts an List in ascending order (lowest to highest) using the merge sort
+	 * algorithm.
+	 * @param lo, the lowest index of the part of the array to be sorted
+	 * @param hi, the highest index of the part of the array to be sorted
+	 */
+	private void mergeSort(int lo, int hi)
+	{
+		if((lo < hi) && (hi - lo)>=1) {
+			ArrayList<Integer> important = new ArrayList<Integer>();
+			ArrayList<Integer> sorted = new ArrayList<Integer>();
+			//midpoint
+			int mid = (lo+hi)/2;
+			
+			//sort first and second halves
+			mergeSort(lo, mid);
+			important.add(lo);
+			important.add(mid);
+			SortingAlgorithmsFunction.drawSortedBars(important,sorted);
+			 try {
+				 Thread.sleep(100);
+			 } catch (InterruptedException e) {
+				 e.printStackTrace();
+		   	 }
+			 important.clear();
+			 
+			mergeSort(mid+1, hi);
+			important.add(mid+1);
+			important.add(hi);
+			SortingAlgorithmsFunction.drawSortedBars(important,sorted);
+			 try {
+				 Thread.sleep(100);
+			 } catch (InterruptedException e) {
+				 e.printStackTrace();
+		   	 }
+			 
+			//merge the sorted halves
+			merge(lo, mid, hi);
+		}
+	}
+}
