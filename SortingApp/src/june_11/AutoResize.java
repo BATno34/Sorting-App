620f37f41a011be06f9b565bb08b2e3b291e17b5
ardav <ardav@192.168.0.10>
2021-06-04 13:45:01 -0400

    javadoc of AutoResize

--- SortingApp/src/june_11/AutoResize.java
+++ SortingApp/src/june_11/AutoResize.java
@@ -12,6 +12,7 @@ public class AutoResize {
 	public static int frameNewWidth;
 	public static int frameNewHeight;
 	
+	//Constructor
 	public AutoResize (int initialX, int initialY, int initialWidth, int initialHeight, int initialFontSize, int initialFrameWidth, int initialFrameHeight, int frameNewWidth, int frameNewHeight) {
 		this.initialX = initialX;
 		this.initialY = initialY;
@@ -24,24 +25,45 @@ public class AutoResize {
 		this.frameNewHeight = frameNewHeight;
 	}
 	
+	/**
+	 * finding the new value of x, fitting the new frame size
+	 * @return new value of x
+	 */
 	public int newX() {
 		return (int)(frameNewWidth * initialX / initialFrameWidth);
 	}
 	
+	/**
+	 * finding new value of y, fitting the new frame size
+	 * @return new value of y
+	 */
 	public int newY() {
 		return (int)(frameNewHeight * initialY / initialFrameHeight);
 	}
 	
+	/**
+	 * finding new width of the component, fitting the new frame size
+	 * @return new width
+	 */
 	public int newWidth() {
 		return (int)(frameNewWidth * initialWidth / initialFrameWidth);
 	}
 	
+	/**
+	 * finding new height of the component, fitting the new frame size
+	 * @return new height
+	 */
 	public int newHeight() {
 		return (int)(frameNewHeight * initialHeight / initialFrameHeight);
 	}
 	
+	/**
+	 * finding new font size, fitting the new frame size
+	 * @return new font size
+	 */
 	public int newFontSize() {
 		return (int)(initialFontSize * newWidth() / initialWidth);
 	}
-}
+	
+}//end of the class
 
