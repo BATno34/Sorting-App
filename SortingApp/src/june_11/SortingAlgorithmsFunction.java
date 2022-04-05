ddac813d22ab32ebf3acb68718b111bd303ad8b0
BATno34 <ardavanalaei@gmail.com>
2022-04-05 11:48:30 -0400

    small error fixed for insertion sort

--- SortingApp/src/june_11/SortingAlgorithmsFunction.java
+++ SortingApp/src/june_11/SortingAlgorithmsFunction.java
@@ -113,7 +113,7 @@ public class SortingAlgorithmsFunction extends JFrame {
 			
 			if (pivotBar.contains(m)) { //already sorted
 				bars[m].setBackground(Color.GREEN);
-			} else if (comparingBars.indexOf(m) != -1){ //being compared
+			} else if (comparingBars.indexOf(m) != -1){ //being compared (for insertion)
 				bars[m].setBackground(Color.BLACK); 
 			} else if (sortedBars.contains(m)) { //already sorted
 				bars[m].setBackground(Color.BLUE);
